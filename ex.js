d47971ff94b67f24c22a81b0c5a637c1fb34f961
Agnieszka Zachariasiewicz <agnieszka.kanarcia@gmail.com>
2023-11-29 04:00:52 +0100

    changed exerything to class syntax

--- ex.js
+++ ex.js
@@ -19,75 +19,108 @@ window.onload = function(){
         }    
         
     }
+    const library = new Library;
     function addBookButtonListener(event) {
         event.preventDefault();
         if(addBookTitleField.value){
-            addBookToLibrary(addBookTitleField.value,addBookAuthorField.value,addBookPagesField.value,addBookReadCheckbox.checked);
+            library.addBookToLibrary(addBookTitleField.value,addBookAuthorField.value,addBookPagesField.value,addBookReadCheckbox.checked);
         }
 
     }
-    myLibrary.forEach(book => book.displayBook()); //this needs testing but to lazy to hardtype some book obj into the array sorry
+    
 }
-//library setup
-var myLibrary = [];
-
-function addBookToLibrary(title,author,pages,read){
-    let position = myLibrary.push(new Book(title,author,pages,read));
-    myLibrary[position-1].displayBook();
+class Library {
+    constructor(books=[]){
+        this.myLibrary = [];
+        for(book of books){
+            addBookToLibrary(...book);    
+        }
+    }
+    addBookToLibrary(title,author,pages,read){
+        const book = new Book(title,author,pages,read);
+        this.myLibrary.push(book);
+        const bookCard = new BookCard(book);
+        bookCard.displayBook();
+    }
 }
 
-//book object + its methods declared on the prototype
-function Book(title,author,noPages,readState){
-    this.title = title;
-    this.author = author;
-    this.noPages = noPages;
-    this.readState = readState||false;
-    Book.prototype.displayBook = function (insertDestination=null){
-            const bookCard = document.createElement("div");
-            bookCard.classList.add("book-card");
-            const bookCardTitle = document.createElement("h3");
-            bookCardTitle.classList.add("title-display");
-            const bookCardAuthor = document.createElement("h4");
-            bookCardAuthor.classList.add("author-display");
-            const bookCardPages = document.createElement("p");
-            const bookCardRead = document.createElement("p");
-            const bookCardDelete = document.createElement("button");
-            bookCardTitle.appendChild(document.createTextNode(this.title));
-            bookCardAuthor.appendChild(document.createTextNode(this.author||"unknown"));
-            bookCardPages.appendChild(document.createTextNode(this.noPages||"unknown"));
-            bookCardRead.appendChild(document.createTextNode((this.readState?"read":"not read")));
-            bookCardDelete.appendChild(document.createTextNode('Delete book'));
-            bookCardDelete.classList.add("delete-button");
-            bookCardRead.classList.add("read-button");
-            if(this.readState){
+class Book {
+    constructor(title, author, noPages, readState) {
+      this.self = this;
+      this.title = title;
+      this.author = author;
+      this.noPages = noPages;
+      this.readState = readState;
+    }
+
+    displayBook(){
+        return {title:this.title,author:this.author,noPages:this.noPages,readState: this.readState}
+    }
+    toggleReadState(){
+        this.readState = !this.readState;
+    }
+}
+
+class BookCard {
+    constructor(book){
+        this.book = book;
+        this.bookCard="";
+        this.self = this;
+    }
+    updateBookCard(bookCard){
+        this.bookCard = bookCard;
+    }
+    displayBook(position='to_front'){
+        const bookCard = document.createElement("div");
+        bookCard.classList.add("book-card");
+        const bookCardTitle = document.createElement("h3");
+        bookCardTitle.classList.add("title-display");
+        const bookCardAuthor = document.createElement("h4");
+        bookCardAuthor.classList.add("author-display");
+        const bookCardPages = document.createElement("p");
+        const bookCardRead = document.createElement("p");
+        const bookCardDelete = document.createElement("button");
+
+        const bookData = this.book.displayBook();
+            bookCardTitle.appendChild(document.createTextNode(bookData.title));
+            bookCardAuthor.appendChild(document.createTextNode(bookData.author||"unknown"));
+            bookCardPages.appendChild(document.createTextNode(bookData.noPages||"unknown"));
+            bookCardRead.appendChild(document.createTextNode((bookData.readState?"read":"not read")));
+        
+        bookCardDelete.appendChild(document.createTextNode('Delete book'));
+        bookCardDelete.classList.add("delete-button");
+        bookCardRead.classList.add("read-button");
+            
+            if(bookData.readState){
                 bookCardRead.classList.add("read");
             }
-            bookCard.appendChild(bookCardTitle);
-            bookCard.appendChild(bookCardAuthor);
-            bookCard.appendChild(bookCardPages);
-            bookCard.appendChild(bookCardRead);
-            bookCard.appendChild(bookCardDelete);
-            bookCardDelete.addEventListener("click", deleteBook, false);
-            function deleteBook() {
-                bookCard.remove();
-            }
-            bookCardRead.addEventListener("click", ()=>{
-               this.changeReadState();
-            });
-            this.bookCard = bookCard;
-            document.getElementsByClassName('library')[0].insertBefore(bookCard,insertDestination);
-    }
-    Book.prototype.changeReadState = function(){
-        if(this.readState){
-            this.readState = false;
-        }
-        else{
-            this.readState = true;
-        }
-        let oldBookCard = this.bookCard; //remembering this cause displayBook will overwrite it
-        this.displayBook(oldBookCard);
-        oldBookCard.remove();
 
+        bookCard.appendChild(bookCardTitle);
+        bookCard.appendChild(bookCardAuthor);
+        bookCard.appendChild(bookCardPages);
+        bookCard.appendChild(bookCardRead);
+        bookCard.appendChild(bookCardDelete);
+
+        bookCardDelete.addEventListener("click", function(){this.deleteBook(bookCard)}.bind(this.self), false);
+        bookCardRead.addEventListener("click", function(e){
+                const position = e.originalTarget.closest(".book-card").nextSibling;
+                this.changeReadStateCallback(bookCard,position)
+            }.bind(this.self));
+        this.updateBookCard(bookCard);
+        if(position === 'to_front'){
+            position = document.getElementsByClassName('library')[0].children[0]
+        }
+        document.getElementsByClassName('library')[0].insertBefore(bookCard,position);
     }
-}
+
+    deleteBook(bookCard){
+        bookCard.remove();
+    }
+    changeReadStateCallback(bookCard,position){
+        this.book.toggleReadState();
+        this.deleteBook(bookCard);
+        this.displayBook(position);
+        
+    }
+}    
 
